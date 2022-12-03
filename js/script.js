1261e2fd04ec25c739d7744280b02e2780ce51ed
Karan Gupta <d22124440@mytudublin.ie>
2022-12-03 22:08:25 +0000

    header done

--- js/script.js
+++ js/script.js
@@ -9,11 +9,58 @@ let searchBtn = document.querySelector("#search-btn");
 searchBtn.addEventListener("click", function () {
   searchForm.classList.toggle("active");
 
-  //   shoppingCart.classList.remove("active");
-  //   loginForm.classList.remove("active");
-  //   navbar.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
 });
 
-// document.querySelector("#search-btn").onclick = () => {
-//   searchForm.classList.toggle("active");
-// };
+// ---------------
+
+// shopping cart box toggle visibility:
+let shoppingCart = document.querySelector(".shopping-cart");
+let cartBtn = document.querySelector("#cart-btn");
+
+// toggle the shopping cart box visibility
+cartBtn.addEventListener("click", function () {
+  shoppingCart.classList.toggle("active");
+  searchForm.classList.remove("active");
+
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+});
+
+// ---------------
+
+// login form box toggle visibility:
+let loginForm = document.querySelector(".login-form");
+let loginBtn = document.querySelector("#login-btn");
+
+// toggle the shopping cart box visibility
+loginBtn.addEventListener("click", function () {
+  loginForm.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+
+  navbar.classList.remove("active");
+});
+
+// ---------------
+
+// menu button box
+let navbar = document.querySelector(".navbar");
+let menuBtn = document.querySelector("#menu-btn");
+
+// toggle the menu navigation visibility
+menuBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+});
+
+window.addEventListener("onscroll", function () {
+  searchForm.classList.remove("active");
+  shoppingCart.classList.remove("active");
+  loginForm.classList.remove("active");
+  navbar.classList.remove("active");
+});
