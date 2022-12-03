035a8d8184b083a7351d3da196e433d1c4d86d48
Karan Gupta <d22124440@mytudublin.ie>
2022-12-03 19:14:18 +0000

    wip

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 // Common JavaScript File for all HTMl files and CSS File
+
+// toggle visibility of search form in header
+
+let searchForm = document.querySelector(".search-form");
+let searchBtn = document.querySelector("#search-btn");
+
+// toggle the search input field visibility
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+
+  //   shoppingCart.classList.remove("active");
+  //   loginForm.classList.remove("active");
+  //   navbar.classList.remove("active");
+});
+
+// document.querySelector("#search-btn").onclick = () => {
+//   searchForm.classList.toggle("active");
+// };
