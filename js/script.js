bb26565cf66a0b4412b7267c1282496fcf99db5f
Karan Gupta <d22124440@mytudublin.ie>
2022-12-05 01:21:51 +0000

    wip review

--- js/script.js
+++ js/script.js
@@ -64,3 +64,28 @@ window.addEventListener("onscroll", function () {
   loginForm.classList.remove("active");
   navbar.classList.remove("active");
 });
+
+// swiper js for slider for review cards
+
+var swiper = new Swiper(".review-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+
+  centeredSlides: true,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
