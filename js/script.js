4205d7dd8c97f04384284eb0242a0ca2d8668159
Karan Gupta <d22124440@mytudublin.ie>
2022-12-05 03:07:31 +0000

    wip home

--- js/script.js
+++ js/script.js
@@ -89,3 +89,52 @@ var swiper = new Swiper(".review-slider", {
     },
   },
 });
+// swiper js for slider for brand number cards
+
+var swiper = new Swiper(".numBox-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+
+  centeredSlides: true,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// swiper js for slider for brand logos cards
+
+var swiper = new Swiper(".brandBox-slider", {
+  loop: true,
+  spaceBetween: 20,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+
+  centeredSlides: true,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1020: {
+      slidesPerView: 5,
+    },
+  },
+});
